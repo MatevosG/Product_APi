21a64426dfae57f6cbab910f33abce9729d857ce
Matevos Grigoryan <matevos.grigoryan@technamin.com>
2022-04-06 18:40:27 +0400

    naturi chem jogum

--- Product_APi/Common/CacheAttribute.cs
+++ Product_APi/Common/CacheAttribute.cs
@@ -1,6 +1,7 @@
 ﻿using BLL.Cache;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Diagnostics.CodeAnalysis;
+using System.Text;
 
 namespace Product_APi.Common
 {
@@ -9,7 +10,18 @@ namespace Product_APi.Common
         //ICacheRepository cacheRepository = new CacheRepository(,);
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            base.OnActionExecuting(context);
+            //var tt = context.HttpContext.RequestServices.GetRequiredService<>
+            var cacheService = context.HttpContext.RequestServices.GetRequiredService<ICacheRepository>();
+
+            // var casheKey = GenerateCachKeyfromRequest(context.HttpContext.Request);
+            var cachedResponse = cacheService.GetCacheResponse();
+
         }
+
+        //private string GenerateCachKeyfromRequest(HttpRequest request)
+        //{
+        //    var keyBuilder = new StringBuilder();
+        //    keyBuilder.Append($"{request.Path}")
+        //}
     }
 }
